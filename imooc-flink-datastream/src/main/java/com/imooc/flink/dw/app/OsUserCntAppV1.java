9133482ba811b9b0b3cc0c801dcf0c5235091268
sweetcs <sweetcs@sweetcsdeMacBook-Pro.local>
2022-05-06 12:43:50 +0800

    m

--- imooc-flink-datastream/src/main/java/com/imooc/flink/dw/app/OsUserCntAppV1.java
+++ imooc-flink-datastream/src/main/java/com/imooc/flink/dw/app/OsUserCntAppV1.java
@@ -11,15 +11,21 @@ import org.apache.flink.client.program.StreamContextEnvironment;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.redis.RedisSink;
+import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommand;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommandDescription;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisMapper;
 
 public class OsUserCntAppV1 {
+
     public static void main(String[] args) throws Exception {
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         DataStreamSource<String> source = env.readTextFile("data-file/access-v2.txt");
 
-        source.map(new MapFunction<String, Access>() {
+        SingleOutputStreamOperator<Tuple3<String, String, Integer>> result = source.map(new MapFunction<String, Access>() {
             @Override
             public Access map(String s) throws Exception {
 
@@ -28,23 +34,52 @@ public class OsUserCntAppV1 {
 
                 return access;
             }
-        }).filter(x->x!=null && x.event.equals("startup"))
-                        .map(new MapFunction<Access, Tuple3<String,String,Integer>>() {
-                            @Override
-                            public Tuple3<String, String, Integer> map(Access access) throws Exception {
+        }).filter(x -> x != null && x.event.equals("startup"))
+                .map(new MapFunction<Access, Tuple3<String, String, Integer>>() {
+                    @Override
+                    public Tuple3<String, String, Integer> map(Access access) throws Exception {
 
-                                return Tuple3.of(access.os,String.valueOf(access.nu),1);
-                                
-                            }
-                        }).keyBy(new KeySelector<Tuple3<String, String, Integer>, Tuple2<String, String>>() {
+                        return Tuple3.of(access.os, String.valueOf(access.nu), 1);
+
+                    }
+                }).keyBy(new KeySelector<Tuple3<String, String, Integer>, Tuple2<String, String>>() {
                     @Override
                     public Tuple2<String, String> getKey(Tuple3<String, String, Integer> in) throws Exception {
-                        return Tuple2.of(in.f0,in.f1);
+                        return Tuple2.of(in.f0, in.f1);
                     }
-                }).sum(2).print();
+                }).sum(2);
+
+
+        FlinkJedisPoolConfig conf = new FlinkJedisPoolConfig.Builder().setHost("127.0.0.1").setPort(6379).build();
+
+        result.addSink(new RedisSink<Tuple3<String, String, Integer>>(conf, new RedisExampleMapper()));
 
 
         env.execute();
 
+    }
+
+
+    static class RedisExampleMapper implements RedisMapper<Tuple3<String, String, Integer>> {
+
+        @Override
+        public RedisCommandDescription getCommandDescription() {
+           return new RedisCommandDescription(RedisCommand.HSET, "os_user-cnt");
+        }
+
+//        @Override
+//        public RedisCommandDescription getCommandDescription() {
+//            return new RedisCommandDescription(RedisCommand.SET);
+//        }
+
+        @Override
+        public String getKeyFromData(Tuple3<String, String, Integer> data) {
+            return data.f0 + "_" + data.f1;
+        }
+
+        @Override
+        public String getValueFromData(Tuple3<String, String, Integer> data) {
+            return data.f2 + "";
+        }
     }
 }
